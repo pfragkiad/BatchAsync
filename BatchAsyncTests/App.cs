ec061038f8db8f5ea7bb0a129d6f986f71bc6064
pfragkiad <pfragkiad@auth.gr>
2022-08-19 21:12:01 +0300

    First commit.

---
+++ BatchAsyncTests/App.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting; //install package for DI
+
+namespace BatchAsyncApp;
+
+public static class App
+{
+    public static IHost GetHost()
+    {
+        var host = Host.CreateDefaultBuilder().ConfigureServices(
+            (context, services) =>
+            {
+                //example adding the batchasync
+                services.AddTransient<IBatchAsync, BatchAsync>();
+            }).Build();
+
+        return host;
+    }
+
+    //example retrieving the batchasync
+    public static IBatchAsync? GetBatchAsync(this IServiceProvider services) =>
+        services.GetService<IBatchAsync>();
+}
